b22d5922970d497bdaeb83d49a21a1db6a434ac7
Daniel <danstad2012@gmail.com>
2019-12-03 13:05:49 -0800

    set up routes

--- api/server.js
+++ api/server.js
@@ -6,4 +6,40 @@ server.get('/', (req,res) => {
   res.send(`<h3>Oh hi there</h3>`)
 })
 
+server.post("/api/posts", (req,res) => {
+  const newPost = req.body
+  console.log(newPost)
+})
+
+server.post("/api/posts/:id/comments", (req,res) => {
+  const newPost = req.body
+  console.log(newPost)
+})
+
+server.get("/api/posts", (req,res) => {
+  const newPost = req.body
+  console.log(newPost)
+})
+
+server.get("/api/posts/:id", (req,res) => {
+  const newPost = req.body
+  console.log(newPost)
+})
+
+server.get("/api/posts/:id/comments", (req,res) => {
+  const newPost = req.body
+  console.log(newPost)
+})
+
+server.delete("/api/posts/:id", (req,res) => {
+  const newPost = req.body
+  console.log(newPost)
+})
+
+server.put("/api/posts/:id", (req,res) => {
+  const newPost = req.body
+  console.log(newPost)
+})
+
+
 module.exports = server

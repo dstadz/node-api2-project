72acdf8bcb2dd3f8a71cbec1bd473dca14fdcb26
Daniel <danstad2012@gmail.com>
2019-12-03 16:43:20 -0800

    finishd comment post?

--- api/server.js
+++ api/server.js
@@ -23,33 +23,23 @@ server.post("/api/posts/:id/comments", (req,res) => {
   const post = db.findById(id)
   const com = req.body
   console.log(com, id, post)
-  const { postId, text } = com
+  const { text } = com
 
 
-  //   - If the _post_ with the specified `id` is not found: -- return HTTP status code `404` (Not Found). -- return the following JSON object: `{ message: "The post with the specified ID does not exist." }`.
-    if(!id) res.status(404).json({ message: "The post with the specified ID does not exist." })
-
-
-// - If the request body is missing the `text` property: -- cancel the request. -- respond with HTTP status code `400` (Bad Request). -- return the following JSON response: `{ errorMessage: "Please provide text for the comment." }`.
-    else if (text) res.status(400).json({ errorMessage: "Please provide text for the comment." })
-
-
-// - If the information about the _comment_ is valid: -- save the new _comment_ the the database. -- return HTTP status code `201` (Created). -- return the newly created _comment_.
-    else res.status(201).json({com})
-
-    db.insertComment(com)
+    if(id) res.status(404).json({ message: "The post with the specified ID does not exist." })
+    else if (!text) res.status(400).json({ errorMessage: "Please provide text for the comment." })
+    else{
+      res.status(201).json(com)
+      db.insertComment(com)
       .then(ment => {
         console.log(ment)
       })
-// - If there's an error while saving the _comment_: -- cancel the request. -- respond with HTTP status code `500` (Server Error). -- return the following JSON object: `{ error: "There was an error while saving the comment to the database" }`.
-  .catch(err => {
-    console.log('goofed with :', err)
-    res.status(500).json({ error: "There was an error while saving the comment to the database" })
+      .catch(err => {
+        console.log('goofed with :', err)
+        res.status(500).json({ error: "There was an error while saving the comment to the database" })
+      })
+    }
   })
-
-
-  
-})
 
 server.get("/api/posts", (req,res) => {
   db.find()
